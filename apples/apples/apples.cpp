5b4926f9959f584ac9afddd90298ed76dc4df13f
SlezaKlouna <knijnaya.lakomka83@gmail.com>
2016-03-22 14:45:47 +0200

    Массивы и задача про яблоки))

---
+++ apples/apples/apples.cpp
@@ -0,0 +1,29 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+#include "stdafx.h"
+#include <cstdlib> 
+int main()
+{
+	int answer = 0;
+
+	scanf_s("%d", &answer);//принимаешь число
+		char art [10];
+		fgets(art, 10, stdin);
+	if (answer == 1)
+	{
+		art [10] = 'A';
+	}
+	else if (answer == 2)
+	{
+		art[10] = 'B';
+	}
+	else if (answer == 3)
+	{
+		art[10] = 'C';
+	}
+	printf("\s", &art);	
+	getchar();
+	system("pause");
+	return 0;
+}
+
